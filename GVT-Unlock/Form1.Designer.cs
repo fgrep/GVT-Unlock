565c22718aff32b480a724247687aceee60773a1
fgrep <fgrep@ipadmin.fsonline.com.br>
2015-06-17 21:12:08 -0300

    Processo de login automatizado Adicionado opcao para bloqueio do modem Adicionado opcao para habilitar SSH

--- GVT-Unlock/Form1.Designer.cs
+++ GVT-Unlock/Form1.Designer.cs
@@ -31,12 +31,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -52,73 +55,89 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(442, 25);
+            this.button2.Location = new System.Drawing.Point(442, 10);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(115, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Iniciar desbloqueio";
+            this.button2.Text = "Desbloquear";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(560, 74);
+            this.panel1.Size = new System.Drawing.Size(560, 101);
             this.panel1.TabIndex = 5;
             // 
-            // label4
+            // checkBox2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(480, 58);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "© 2015 fgrep";
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(107, 78);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(226, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Habilitar TV (Não testado em modo bridge)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Visible = false;
             // 
-            // label3
+            // button1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 43);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(330, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "- Quando solicitado, reinicie o modem e aguarde cerca de 5 minutos.";
+            this.button1.Location = new System.Drawing.Point(442, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Bloquear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 78);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Habilitar SSH";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 26);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(390, 13);
+            this.label2.Size = new System.Drawing.Size(315, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "- Aguarde a página de login carregar, faça o login e clique em iniciar desbloquei" +
-    "o.";
+            this.label2.Text = "- Aguarde o logo da GVT ser exibido e clique na opção desejada.";
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.webBrowser1);
             this.panel2.Controls.Add(this.textBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 74);
+            this.panel2.Location = new System.Drawing.Point(0, 101);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(560, 243);
+            this.panel2.Size = new System.Drawing.Size(560, 216);
             this.panel2.TabIndex = 6;
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Location = new System.Drawing.Point(0, 124);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.ScriptErrorsSuppressed = true;
-            this.webBrowser1.Size = new System.Drawing.Size(560, 243);
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(560, 92);
             this.webBrowser1.TabIndex = 1;
             // 
             // textBox1
@@ -127,8 +146,27 @@
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(560, 243);
+            this.textBox1.Size = new System.Drawing.Size(560, 216);
             this.textBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "*** O desbloqueio pode afetar o serviço de TV!";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(442, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -137,9 +175,10 @@
             this.ClientSize = new System.Drawing.Size(560, 317);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Pace v5471 Unlock";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GVT Pace v5471 Unlock - v0.4";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -157,8 +196,11 @@
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- GVT-Unlock/Form1.cs
+++ GVT-Unlock/Form1.cs
@@ -20,17 +20,8 @@ namespace GVT_Unlock
         public Form1()
         {
             InitializeComponent();
-            
-            var certificate = new X509Certificate2("cert.p12", "");
-            var listener = (SecureHttpListener)HttpListener.Create(IPAddress.Any, 443, certificate);
-
-            GlobalVar.UnlockSent = 0;
-            listener.UseClientCertificate = true;
-            listener.RequestReceived += OnRequest;
-            listener.Start(5);
-            AppendTextBox("Servidor web iniciado.\r\n");
             AppendTextBox("Carregando página de login.\r\n");
-            webBrowser1.Navigate("http://192.168.25.1/pt_BR/admin/config_lan.htm");
+            webBrowser1.Navigate("http://192.168.25.1/pt_BR/admin/config_lan.htm");            
         }
 
         public void AppendTextBox(string value)
@@ -43,14 +34,28 @@ namespace GVT_Unlock
             textBox1.Text += value;
         }
 
-        public void OnRequest(object sender, RequestEventArgs e)
+        public void OnRequestDesbloqueio(object sender, RequestEventArgs e)
         {
             // Write info to the buffer.
             e.Response.Connection.Type = ConnectionType.Close;
             if (GlobalVar.UnlockSent == 0)
             {
-                AppendTextBox("Modem conectado, enviando parametros para habilitar SSH e tipo de acesso 4 (desbloqueio)\r\n");
-                byte[] buffer = Encoding.Default.GetBytes("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:cwmp=\"urn:dslforum-org:cwmp-1-0\"><soap:Header><cwmp:NoMoreRequests>0</cwmp:NoMoreRequests></soap:Header><soap:Body><cwmp:SetParameterValues><ParameterList encodingStyle:arrayType=\"cwmp:ParameterValueStruct[2]\"><ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.Services.GvtConfig.AccessClass</Name><Value>4</Value></ParameterValueStruct><ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.Services.SSH.Enable</Name><Value>1</Value></ParameterValueStruct></ParameterList><ParameterKey></ParameterKey></cwmp:SetParameterValues></soap:Body></soap:Envelope>");
+                string opcionais = "";
+                int numparams = 1;
+                if (checkBox1.Checked)
+                {
+                    opcionais += "<ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.Services.SSH.Enable</Name><Value>1</Value></ParameterValueStruct>";
+                    numparams++;
+                }
+
+                if (checkBox2.Checked)
+                {
+                    opcionais += "<ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.HPNA.Enable</Name><Value>1</Value></ParameterValueStruct>";
+                    numparams++;
+                }
+
+                AppendTextBox("Modem conectado, enviando configuração.\r\n");
+                byte[] buffer = Encoding.Default.GetBytes("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:cwmp=\"urn:dslforum-org:cwmp-1-0\"><soap:Header><cwmp:NoMoreRequests>0</cwmp:NoMoreRequests></soap:Header><soap:Body><cwmp:SetParameterValues><ParameterList encodingStyle:arrayType=\"cwmp:ParameterValueStruct["+numparams+"]\"><ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.Services.GvtConfig.AccessClass</Name><Value>4</Value></ParameterValueStruct>"+opcionais+"</ParameterList><ParameterKey></ParameterKey></cwmp:SetParameterValues></soap:Body></soap:Envelope>");
                 e.Response.Body.Write(buffer, 0, buffer.Length);
                 GlobalVar.UnlockSent = 1;
             }
@@ -63,48 +68,126 @@ namespace GVT_Unlock
             }
         }
 
-        /*
-        static string ConfigurarModem(string cookie)
+        public void OnRequestBloqueio(object sender, RequestEventArgs e)
         {
-            using (var client = new WebClientEx())
+            // Write info to the buffer.
+            e.Response.Connection.Type = ConnectionType.Close;
+            if (GlobalVar.UnlockSent == 0)
             {
-                var values = new NameValueCollection
+                string opcionais = "";
+                int numparams = 1;
+                if (checkBox1.Checked)
+                {
+                    opcionais += "<ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.Services.SSH.Enable</Name><Value>1</Value></ParameterValueStruct>";
+                    numparams++;
+                }
+                else
+                {
+                    opcionais += "<ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.Services.SSH.Enable</Name><Value>0</Value></ParameterValueStruct>";
+                    numparams++;
+                }
+
+                AppendTextBox("Modem conectado, enviando configuração.\r\n");
+                byte[] buffer = Encoding.Default.GetBytes("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:cwmp=\"urn:dslforum-org:cwmp-1-0\"><soap:Header><cwmp:NoMoreRequests>0</cwmp:NoMoreRequests></soap:Header><soap:Body><cwmp:SetParameterValues><ParameterList encodingStyle:arrayType=\"cwmp:ParameterValueStruct["+numparams+"]\"><ParameterValueStruct><Name>InternetGatewayDevice.Services.X_Pace_Com.Services.GvtConfig.AccessClass</Name><Value>2</Value></ParameterValueStruct>" + opcionais + "</ParameterList><ParameterKey></ParameterKey></cwmp:SetParameterValues></soap:Body></soap:Envelope>");
+                e.Response.Body.Write(buffer, 0, buffer.Length);
+                GlobalVar.UnlockSent = 1;
+            }
+            else if (GlobalVar.UnlockSent == 1)
             {
-                { "login", "admin" },
-                { "password", "gvt12345" },
-                { "request", "authentication"},
-                { "level", "admin" },
-                { "seed", "c1e550c8801aba24b71b33ce360975a769b8aaa6030ad089a67086305289bb44" },
-            };
-                string url = "http://192.168.25.1/pt_BR/admin/config_lan.htm";
-                client.UploadValues("http://192.168.25.1/cgi-bin/login.cgi", values);
-                //client.CookieContainer.SetCookies(url, cookie);
-                return client.DownloadString("http://192.168.25.1/pt_BR/admin/config_lan.htm");
+                byte[] buffer = Encoding.Default.GetBytes("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"               xmlns:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"               xmlns:cwmp=\"urn:dslforum-org:cwmp-1-0\">  <soap:Header>    <cwmp:NoMoreRequests>0</cwmp:NoMoreRequests>  </soap:Header>  <soap:Body>    <cwmp:InformResponse>      <MaxEnvelopes>1</MaxEnvelopes>    </cwmp:InformResponse>  </soap:Body></soap:Envelope>");
+                e.Response.Body.Write(buffer, 0, buffer.Length);
+                GlobalVar.UnlockSent = 2;
+                AppendTextBox("Finalizado.\r\n");
             }
         }
-        */
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string strSource = webBrowser1.DocumentText;
-            string strStart = "token: util.parseJson(\'\"";
-            if (strSource.Contains(strStart))
+            var certificate = new X509Certificate2("cert.p12", "");
+            var listener = (SecureHttpListener)HttpListener.Create(IPAddress.Any, 443, certificate);
+
+            GlobalVar.UnlockSent = 0;
+            listener.UseClientCertificate = true;
+            listener.RequestReceived += OnRequestDesbloqueio;
+            listener.Start(5);
+
+            AppendTextBox("Servidor web iniciado.\r\n");
+
+            webBrowser1.Document.GetElementById("login").SetAttribute("value", "admin");
+            webBrowser1.Document.GetElementById("password").SetAttribute("value", "gvt12345");
+            webBrowser1.Document.InvokeScript("checkSubmit");
+            HtmlElement form = webBrowser1.Document.GetElementById("login_form");
+            if (form != null)
+                form.InvokeMember("submit");
+            webBrowser1.Navigated += webBrowser1_Navigated_1;
+        }
+
+        void webBrowser1_Navigated_1(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            if (webBrowser1.Url.ToString() == "http://192.168.25.1/pt_BR/admin/config_lan.htm")
             {
-                int Start = strSource.IndexOf(strStart, 0) + strStart.Length;
-                var token = strSource.Substring(Start, 6);
-                webBrowser1.Hide();
-                AppendTextBox("Token encontrado: " + token + "\r\n");
-                AppendTextBox("Apontando host acs.gvt.com.br para 192.168.25.200\r\n");
-                webBrowser1.Navigate("http://192.168.25.1/cgi-bin/generic.cgi?token=" + token + "&write=LANDevice_1_HostConfig_StaticHost_1_IPAddress:192.168.25.200&write=LANDevice_1_HostConfig_StaticHost_1_Hostname:acs.gvt.com.br");
-                AppendTextBox("Aguarde 5 segundos e reinicie o modem para concluir o processo.\r\n");
+                string strSource = webBrowser1.DocumentText;
+                string strStart = "token: util.parseJson(\'\"";
+                if (strSource.Contains(strStart))
+                {
+                    int Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                    var token = strSource.Substring(Start, 6);
+                    AppendTextBox("Token encontrado: " + token + "\r\n");
+                    AppendTextBox("Apontando host acs.gvt.com.br para 192.168.25.200\r\n");
+                    webBrowser1.Navigate("http://192.168.25.1/cgi-bin/generic.cgi?token=" + token + "&write=LANDevice_1_HostConfig_StaticHost_1_IPAddress:192.168.25.200&write=LANDevice_1_HostConfig_StaticHost_1_Hostname:acs.gvt.com.br");
+                    AppendTextBox("Reiniciando o modem para concluir o processo ...\r\n");
+                    webBrowser1.Navigate("http://192.168.25.1/pt_BR/admin/resets.htm");
+                }
+                else
+                {
+                    MessageBox.Show("Token não encontrado, feche o programa e inicie o processo novamente.");
+                }
             }
-            else
+            else if (webBrowser1.Url.ToString() == "http://192.168.25.1/pt_BR/admin/resets.htm")
             {
-                MessageBox.Show("Token não encontrado, tente novamente.");
+                System.Threading.Thread.Sleep(5000);
+                string strSource = webBrowser1.DocumentText;
+                string strStart = "token: util.parseJson(\'\"";
+                if (strSource.Contains(strStart))
+                {
+                    int Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                    var token = strSource.Substring(Start, 6);
+                    AppendTextBox("Token encontrado: " + token + "\r\n");
+                    webBrowser1.Navigate("http://192.168.25.1/cgi-bin/generic.cgi?token=" + token + "&fct=reboot");
+                    AppendTextBox("Modem reiniciado. Aguarde ...");
+                }
+                else
+                {
+                    MessageBox.Show("Token não encontrado, reinicie o modem manualmente.");
+                }
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var certificate = new X509Certificate2("cert.p12", "");
+            var listener = (SecureHttpListener)HttpListener.Create(IPAddress.Any, 443, certificate);
 
+            GlobalVar.UnlockSent = 0;
+            listener.UseClientCertificate = true;
+            listener.RequestReceived += OnRequestBloqueio;
+            listener.Start(5);
+
+            AppendTextBox("Servidor web iniciado.\r\n");
+
+            webBrowser1.Document.GetElementById("login").SetAttribute("value", "admin");
+            webBrowser1.Document.GetElementById("password").SetAttribute("value", "gvt12345");
+            webBrowser1.Document.InvokeScript("checkSubmit");
+            HtmlElement form = webBrowser1.Document.GetElementById("login_form");
+            if (form != null)
+                form.InvokeMember("submit");
+            webBrowser1.Navigated += webBrowser1_Navigated_1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 
     public static class GlobalVar
@@ -122,26 +205,46 @@ namespace GVT_Unlock
             }
         }
     }
-    /*
-    public class WebClientEx : WebClient
-    {
-        public CookieContainer CookieContainer { get; set; }
 
-        public WebClientEx()
+    public class CookieAwareWebClient : WebClient
+    {
+        public void Login(string loginPageAddress, NameValueCollection loginData)
         {
-            CookieContainer = new CookieContainer();
+            CookieContainer container;
+
+            var request = (HttpWebRequest)WebRequest.Create(loginPageAddress);
+
+            request.Method = "POST";
+            request.ContentType = "multipart/form-data";
+            var buffer = Encoding.ASCII.GetBytes(loginData.ToString());
+            request.ContentLength = buffer.Length;
+            var requestStream = request.GetRequestStream();
+            requestStream.Write(buffer, 0, buffer.Length);
+            requestStream.Close();
+
+            container = request.CookieContainer = new CookieContainer();
+
+            var response = request.GetResponse();
+            response.Close();
+            CookieContainer = container;
         }
+
+        public CookieAwareWebClient(CookieContainer container)
+        {
+            CookieContainer = container;
+        }
+
+        public CookieAwareWebClient()
+            : this(new CookieContainer())
+        { }
+
+        public CookieContainer CookieContainer { get; private set; }
 
         protected override WebRequest GetWebRequest(Uri address)
         {
-            var request = base.GetWebRequest(address);
-            if (request is HttpWebRequest)
-            {
-                (request as HttpWebRequest).CookieContainer = CookieContainer;
-            }
+            var request = (HttpWebRequest)base.GetWebRequest(address);
+            request.CookieContainer = CookieContainer;
             return request;
         }
     }
-     */
-
  }
